5f04645333c8aab354ff33349a19b9db4de14a92
balacmi <milos.balac@ivt.baug.ethz.ch>
2017-08-07 15:41:52 +0200

    adding car routing code

--- GoogleRouting/src/routing/GoogleCarRouting.java
+++ GoogleRouting/src/routing/GoogleCarRouting.java
@@ -14,66 +14,86 @@ import com.google.maps.model.DirectionsLeg;
 import com.google.maps.model.DirectionsRoute;
 import com.google.maps.model.TravelMode;
 
+
+
 public class GoogleCarRouting {
 
-	
+	/**
+	 * 
+	 * @param args
+	 * 
+	 * The arguments should be in teh following order:
+	 * APIKey
+	 * date [year/month/day]
+	 * input file path
+	 * output file path
+	 * index of trip id [first column index is 0]
+	 * startCoord X index
+	 * startCoord Y index
+	 * endCoord X index
+	 * endCoord Y index
+	 * index of start time in minutes
+	 * 
+	 * Coordinates should be in WGS84 format
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
-		BufferedReader reader = new BufferedReader(new FileReader("C:\\LocalDocuments\\Projects\\PCW\\Routing\\clean_weg_correction1_geocoded.csv"));
+		BufferedReader reader = new BufferedReader(new FileReader(args[2]));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(args[3]));
+		
+		GeoApiContext context = new GeoApiContext.Builder()
+			    .apiKey(args[0])
+			    .build();
+		
+		String year = args[1].split("\\")[0];
+		String month = args[1].split("\\")[1];
+		String day = args[1].split("\\")[2];
+
+		int indexId = Integer.parseInt(args[4]);
+		int indexStartCoordX = Integer.parseInt(args[5]);
+		int indexStartCoordY = Integer.parseInt(args[6]);
+		int indexEndCoordX = Integer.parseInt(args[7]);
+		int indexEndCoordY = Integer.parseInt(args[8]);
+		int indexTime = Integer.parseInt(args[9]);
+
 		reader.readLine();
-		final BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\LocalDocuments\\Projects\\PCW\\Routing\\clean_weg_correction1_car.csv"));
 		String s = reader.readLine();
-		GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyBEkVBy4u7UVyvavAfzoeQmczD_CdGw7pA");
 		
-		int i = 1;
 		while (s != null) {
-			 String[] arr = s.split(";");
-			 if (!arr[1].equals("-99")) {
-					double m = Integer.parseInt(arr[12]);
+			
+			String[] arr = s.split(";");
+			//time in minutes
+			double m = Integer.parseInt(arr[indexTime]);
 			 
-			 int h = (int) (m / 60);
-			 int min = (int)m - h * 60;
-				DateTime time = new DateTime(2016, 12, 29, h, min, DateTimeZone.getDefault());
-
-				
-
-			
-			
+			int h = (int) (m / 60);
+			int min = (int)m - h * 60;
+			DateTime time = new DateTime(Integer.parseInt(year), 
+					Integer.parseInt(month), Integer.parseInt(day),
+					h, min, DateTimeZone.getDefault());			
 		
-			double distance = 0.0;
-					
+			double distance = 0.0;		
+			double travelTime = 0.0;
 	
-					double travelTime = 0.0;
-					DirectionsRoute[] route = DirectionsApi.getDirections(context, arr[1] + " " + arr[2] ,
-							arr[3] + " " + arr[4]).mode(TravelMode.DRIVING).departureTime(time).await();
-					if (route == null || route.length == 0) {
-//						System.out.println("");
-						writer.write(arr[0] + ";-99;-99");
-						writer.newLine();
-					}
-					else if (route.length > 0){
-					for (DirectionsLeg l : route[0].legs) {
-						
-						travelTime += l.duration.inSeconds;
-						distance += l.distance.inMeters;
-					}
-					
-					writer.write(arr[0] + ";" +  travelTime + ";"+ distance);
+			DirectionsRoute[] route = (DirectionsApi.getDirections(context, arr[indexStartCoordX] + " " + arr[indexStartCoordY] ,
+				arr[indexEndCoordX] + " " + arr[indexEndCoordY]).mode(TravelMode.DRIVING).departureTime(time).await()).routes;
+			if (route == null || route.length == 0) {
+					writer.write(arr[indexId] + ";-99;-99");
 					writer.newLine();
-					}
+				}
+			else if (route.length > 0) {
+				for (DirectionsLeg l : route[0].legs) {
+					
+					travelTime += l.duration.inSeconds;
+					distance += l.distance.inMeters;
+				}
 				
+				writer.write(arr[indexId] + ";" +  travelTime + ";"+ distance);
+				writer.newLine();
+			}				
 
-			
-			 }
-		
-		else {
-			writer.write(i + ";-99;-99");
-			writer.newLine();
-			
+			s = reader.readLine();
 		}
-			 i++;
-				s = reader.readLine();
-
-		}
+				
 		writer.flush();
 		writer.close();
 		reader.close();
