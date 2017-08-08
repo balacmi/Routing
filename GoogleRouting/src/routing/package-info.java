1fc002285c7ebcfd981a8aacf393644dd1038217
balacmi <milos.balac@ivt.baug.ethz.ch>
2017-08-08 15:37:58 +0200

    Added coumentation file

---
+++ GoogleRouting/src/routing/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * The purpose of this package is to provide google based routing of OD pairs with different modes.
+ * 
+ * The input files should be a csv or txt file, with a header and ; separated values.
+ * 
+ * The output file contains the following information:
+ * trip id, travel time, travel distance, true/false if the trip was routed with the requested mode, transfer time, 
+ * number of transfers, in-vehicle time
+ * 
+ * transfer time,  number of transfers, in-vehicle time are only used for transit, for other modes it is set to -99
+ */
+/**
+ * @author balacm
+ *
+ */
+package routing;
